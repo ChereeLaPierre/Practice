24f00b84462e89f2f9ab1b5d702aec3660540fc6
Cheree LaPierre <clapierre19@my.whitworth.edu>
2016-11-15 12:58:22 -0800

    1st try

---
+++ whaaaaaa/whaaaaaa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  whaaaaaa
+//
+//  Created by Cheree LaPierre on /1115/16.
+//  Copyright © 2016 Cheree LaPierre. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This should hopefully work";
+}
